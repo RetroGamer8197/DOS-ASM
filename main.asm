a47fa16ef42b034df6a7eaa4d0af8cf872c939cc
RetroGamer8197 <135149664+RetroGamer8197@users.noreply.github.com>
2024-05-26 17:19:41 +0100

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,28 @@
+[ORG 0x7C00]
+xor ax, ax
+mov ds, ax
+cld
+
+mov si, 0x3FFE
+mov [si], dx
+
+jmp start
+
+VersionName db 13, 10, "Totally MS-DOS 6.22", 0
+
+%include "bootloader.asm"
+
+nextSector:
+    mov si, continueText
+    cld
+    call printText
+    ret
+
+%include "functions/cmd.asm"
+%include "functions/display.asm"
+%include "functions/keyboard.asm"
+%include "functions/string.asm"
+
+%include "data/maindata.inc"
+
+times 4096 - ($-$$) db 0
